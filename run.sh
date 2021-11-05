e615f18ffe8036c7e2a6ad026b27933704d3f852
Chiro Liang <Chiro2001@163.com>
2021-11-05 17:27:37 +0800

    run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+export PYTHONUNBUFFERED=1
+export DISPLAY=:0.0
+export MVCAM_SDK_PATH=/opt/MvCamCtrlSDK
+export LD_LIBRARY_PATH=/opt/MvCamCtrlSDK/lib/armhf
+export MVCAM_COMMON_RUNENV=/opt/MvCamCtrlSDK/lib
+
+python3 main.py
+
