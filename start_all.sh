9c84a4d4ae486961592bef13552fef9c802581e4
Chiro <Chiro2001@163.com>
2021-11-07 15:17:01 +0800

    enter

--- start_all.sh
+++ start_all.sh
@@ -4,7 +4,10 @@ cd /home/pi/nuedc
 ./run.sh &
 ssh 192.168.137.232 "sh /home/pi/nuedc/run.sh" &
 
-ssh 192.168.137.231 "sh /home/pi/nuedc/run.sh"
+ssh 192.168.137.231 "sh /home/pi/nuedc/run.sh" &
+
+echo "Press enter to stop"
+read
 
 ssh 192.168.137.231 "sh /home/pi/nuedc/stop.sh" &
 ssh 192.168.137.232 "sh /home/pi/nuedc/stop.sh" &
