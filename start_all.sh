8e2308227066ca1f914def0d2b111782a457ad30
Chiro <Chiro2001@163.com>
2021-11-07 14:38:49 +0800

    start_all

--- start_all.sh
+++ start_all.sh
@@ -7,9 +7,9 @@ ssh 192.168.137.232 "cd /home/pi/nuedc; ./run.sh & echo $! > run.pid" &
 sleep 1
 ssh 192.168.137.231 "cd /home/pi/nuedc; ./run.sh & echo $! > run.pid" &
 
-fg 1
+fg %1
 
-ssh 192.168.137.231 "cd /home/pi/nuedc; ./stop.sh"
-ssh 192.168.137.232 "cd /home/pi/nuedc; ./stop.sh"
+ssh 192.168.137.231 "cd /home/pi/nuedc; sh stop.sh"
+ssh 192.168.137.232 "cd /home/pi/nuedc; sh stop.sh"
 
 
