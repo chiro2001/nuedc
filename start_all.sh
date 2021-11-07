9f539471aa52530421489c25ce0bada6d7d08893
Chiro <Chiro2001@163.com>
2021-11-07 14:56:18 +0800

    enter

--- start_all.sh
+++ start_all.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env zsh
 
 cd /home/pi/nuedc
-#sleep 1
+./run.sh &
 ssh 192.168.137.232 "sh /home/pi/nuedc/run.sh" &
-#sleep 1
-ssh 192.168.137.231 "sh /home/pi/nuedc/run.sh" &
-#fg %1
 
-./run.sh
+ssh 192.168.137.231 "sh /home/pi/nuedc/run.sh"
 
 ssh 192.168.137.231 "sh /home/pi/nuedc/stop.sh"
 ssh 192.168.137.232 "sh /home/pi/nuedc/stop.sh"
+sh stop.sh
 
 
